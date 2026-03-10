f5e1e13e93cc3fc1207a56bb182c2dcd86df71ad
Jitse De Smet <jitse@reedonk8.be>
2026-03-10 16:03:58 +0100

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# OR-Set RDF-datastore
+
+Exposes an RDF store, compatible with `@rdfjs/types` store that implements an OR-set CRDT instantiated with RDF 1.2.
+
+## Install
+
+```bash
+$ yarn add or-set-rdf-store
+```
+
---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    '/test/',
+    '/node_modules/',
+  ],
+  coverageProvider: 'babel',
+  moduleFileExtensions: [
+    'ts',
+    'js',
+  ],
+  testEnvironment: 'node',
+  testMatch: [
+    '<rootDir>/test/**/*-test.ts',
+  ],
+  transform: {
+    '\\.ts$': [ 'ts-jest', {
+      // Enabling this can fix issues when using prereleases of typings packages
+      // isolatedModules: true
+    }],
+  },
+  // The default test timeout is not enough for engine tests, but is enough for packages
+  testTimeout: 20_000,
+};
+
+export default config;
